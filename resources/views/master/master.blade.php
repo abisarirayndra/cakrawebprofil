bf149c5acb544dc514d81c75b4968af8960e055b
abisarirayndra <abi.sarirayndra@gmail.com>
2022-01-26 13:55:09 +0700

    penambahan metadata

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Cakra Krisna Manggala</title>
+        <meta name="description" content="Pusat bimbingan belajar persiapan masuk TNI/Polri dan Kedinasan" />
+        <meta name="author" content="LKP Cakra Krisna Manggala" />
+        <title>Cakra Krisna Manggala - Pusat Bimbingan Belajar Persiapan Masuk TNI Polri dan Kedinasan</title>
         <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
